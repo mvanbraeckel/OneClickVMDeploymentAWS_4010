52fb2f6452bbe92abc758ad30f7cea27bf2b145e
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-01-17 22:43:57 -0500

    oops

--- awsConfigureSet.sh
+++ awsConfigureSet.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # to make resetting aws creds a little easier (copy-paste values here)
-aws_access_key_id=ASIAVFB5VEIYK5KCKLOU
-aws_secret_access_key=zK178kY0Dm3BCWstA+nQIvhqpESqX1piecLkg3tW
+aws_access_key_id=LOL
+aws_secret_access_key=XD
 aws_session_token=FwoGZXIvYXdzEBoaDHX+NZKnCBTHJoK8EiLEAR4EykdivBgXXyxHahYJnFh7ZAP5sKv1TXwXmpexg88qFmxs8B3BSB98mVzbXJonTIrIfhIWtGflI1dJKuoVwlCyqBWzABm1QFKnPg3JXgtAE61i46yW3uj9VTRAtdKi1HEjFrFS3zcVhHLAzdybg8xlHKI0GuNx/PHFMDmJ1VaBdOJIl2RaiIDk5SgN5t4pdfyxdZ3aIKdF4vFfLIoDGbg0IaCmjGZNPt8SZyDAJ2QrQC0YRPC0SlL0f6fzcODuyIhLVocos4Dx/AUyLThXCK37qIpcD3vkI/h41j58BXFzcjLnp5k+jVzigldh78NZEwKcqcPsrE/AIg==
 
 # in order: set aws credentials config for key, secret, session token
